43424864fbf095b11ae63663b47251ec5096f45c
jimmynicol <james@fundly.com>
2014-01-28 16:11:34 -0800

    Refactoring of gulpfile and versioning

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,8 @@
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-    rename = require('gulp-rename'),
-    replace = require('gulp-replace'),
-    minifyCSS = require('gulp-minify-css'),
-    open = require('gulp-open'),
-    clean = require('gulp-clean'),
-    ejs = require('gulp-ejs');
+    p =    require('gulp-load-plugins')({camelize: true}),
+    pkg =  require('./package.json');
 
-gulp.task('styles', ['generateIconFiles'], function(){
-  return gulp.src('vendor/assets/stylesheets/fundly-icon-font-dist.sass')
-    .pipe(sass({ style: 'expanded' }))
-    .pipe(rename('fundly-icon-font.css'))
-    .pipe(gulp.dest('dist/'))
-    .pipe(minifyCSS())
-    .pipe(rename('fundly-icon-font.min.css'))
-    .pipe(gulp.dest('dist/'));
-});
-
-gulp.task('minify', function(){
-  // return gulp.src('vendor/assets/stylesheets/fundly-icon-font-dist.sass')
-  //   .pipe(sass({ style: 'expanded' }))
-  //   .pipe(minifyCSS())
-  //   .pipe(rename('fundly-icon-font.min.css'))
-  //   .pipe(gulp.dest('dist/'));
-});
-
-gulp.task('moveFonts', function(){
-  gulp.src(['src/fonts/*'])
-    .pipe(gulp.dest('vendor/assets/fonts'));
-  return gulp.src(['src/fonts/*'])
-    .pipe(gulp.dest('dist/fonts'));
-});
-
-gulp.task('moveAssets', function(){
-  return gulp.src(['src/demo-files/*'])
-    .pipe(gulp.dest('dist/demo-files'));
-});
-
-gulp.task('replace', function(){
-  return gulp.src('src/demo.html')
-    .pipe(replace(/icon-/g, 'f-icon-'))
-    .pipe(replace(/href="style.css"/, 'href="fundly-icon-font.css"'))
-    .pipe(rename('index.html'))
-    .pipe(gulp.dest('dist/'));
-});
-
-gulp.task('clean', function(){
-  return gulp.src(['dist/', 'vendor/assets/fonts/'], {read: false})
-    .pipe(clean());
-});
-
-gulp.task('open', function(){
-  gulp.src('dist/index.html')
-    .pipe(open("<%file.path%>"), {app: 'google-chrome'});
-});
-
+// Parse the src stylesheet from IcoMoon and pull out the icon keys and content
 gulp.task('generateIconFiles', function(){
   var fs = require('fs'),
       str = fs.readFileSync('src/style.css'),
@@ -74,25 +22,85 @@ gulp.task('generateIconFiles', function(){
   }
 
   gulp.src('vendor/assets/templates/variables.ejs')
-    .pipe(ejs({
+    .pipe(p.ejs({
       icons: icons,
-      contents: contents
+      contents: contents,
+      version: pkg.version
     }))
-    .pipe(rename('_variables.sass'))
+    .pipe(p.rename('_variables.sass'))
     .pipe(gulp.dest('vendor/assets/stylesheets/fundly-icon-font/'));
 
   return gulp.src('vendor/assets/templates/icons.ejs')
-    .pipe(ejs({
+    .pipe(p.ejs({
       icons: icons,
       contents: contents
     }))
-    .pipe(rename('_icons.sass'))
+    .pipe(p.rename('_icons.sass'))
     .pipe(gulp.dest('vendor/assets/stylesheets/fundly-icon-font/'));
+});
+
+// Build the CSS files from SASS, waiting first for the icons to be generated
+gulp.task('styles', ['generateIconFiles'], function(){
+  return gulp.src('vendor/assets/stylesheets/fundly-icon-font-dist.sass')
+    .pipe(p.rubySass({ style: 'expanded' }))
+    .pipe(p.rename('fundly-icon-font.css'))
+    .pipe(gulp.dest('dist/'))
+    .pipe(p.minifyCss())
+    .pipe(p.rename('fundly-icon-font.min.css'))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('moveFonts', function(){
+  gulp.src(['src/fonts/*'])
+    .pipe(gulp.dest('vendor/assets/fonts'));
+  return gulp.src(['src/fonts/*'])
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('moveAssets', function(){
+  return gulp.src(['src/demo-files/*'])
+    .pipe(gulp.dest('dist/demo-files'));
+});
+
+gulp.task('replace', function(){
+  return gulp.src('src/demo.html')
+    .pipe(p.replace(/icon-/g, 'f-icon-'))
+    .pipe(p.replace(/href="style.css"/, 'href="fundly-icon-font.css"'))
+    .pipe(p.rename('index.html'))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('clean', function(){
+  return gulp.src(['dist/', 'vendor/assets/fonts/'], {read: false})
+    .pipe(p.clean());
+});
+
+gulp.task('open', function(){
+  gulp.src('dist/index.html')
+    .pipe(p.open("<%file.path%>"), {app: 'google-chrome'});
 });
 
 gulp.task('build', function(){
   gulp.run(
-    'clean', 'styles', 'minify',
-    'moveFonts', 'moveAssets', 'replace', 'open'
+    'clean', 'styles', 'moveFonts', 'moveAssets', 'replace', 'open'
   );
 });
+
+// Bump Version Numbers
+function bumpVersion (type) {
+  type = type || 'patch';
+
+  gulp.src(['./bower.json', './package.json'])
+    .pipe(bump({type: type}))
+    .pipe(gulp.dest('./'));
+
+  var pkg = require('./package.json');
+
+  gulp.src('lib/fundly/icon/font/version.rb')
+    .pipe(p.replace(/VERSION.*\n/g, 'VERSION = "' + pkg.version + '"\n'))
+    .pipe(gulp.dest('lib/fundly/icon/font/'));
+}
+
+gulp.task('bump:major', function(){ bumpVersion('major'); });
+gulp.task('bump:minor', function(){ bumpVersion('minor'); });
+gulp.task('bump:patch', function(){ bumpVersion('patch'); });
