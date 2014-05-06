df612fa782986670ddf0c1f7e02f773fc213aee6
John Chow <john@fundly.com>
2014-05-06 16:51:24 -0700

    Added group :all (to be loaded in all environments)

--- lib/fundly/icon/font/engine.rb
+++ lib/fundly/icon/font/engine.rb
@@ -2,7 +2,7 @@ module Fundly
   module Icon
     module Font
       class Engine < ::Rails::Engine
-        initializer 'fundly-icon-font.assets.precompile' do |app|
+        initializer 'fundly-icon-font.assets.precompile', :group => :all do |app|
           app.config.assets.precompile += %w(
             fundly-icons.eot fundly-icons.svg
             fundly-icons.ttf fundly-icons.woff)
